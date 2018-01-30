3a71054be064686b7d6566f80dbdb19c87670e68
Conor Schaefer <conor@freedom.press>
2018-01-30 13:43:05 -0800

    Flake8 linting of Python test suite
    Mostly whitespace violations, cleaned up with consistent indentation
    throughout. All files under the tests/ directory now pass flake8
    linting.
    
    There are a few minor exceptions added, to permit lines longer than 80
    characters to aid in readability, e.g. filepaths.
    
    Found one major error, in the decryption tests: two tests sharing the same
    name, so there was clobbering going on. Resolved. Hurray, linting!

--- tests/base.py
+++ tests/base.py
@@ -1,7 +1,3 @@
-from distutils import spawn
-
-import os
-import re
 import subprocess
 import time
 import unittest
@@ -10,58 +6,63 @@ from qubesadmin import Qubes
 
 # base class for per-VM testing
 
+
 class SD_VM_Local_Test(unittest.TestCase):
 
-  def setUp(self):
-    self.app = Qubes()
-    self.vm = self.app.domains[self.vm_name]
-    #self._reboot()
-    if self.vm.is_running():
-      pass
-    else:
-      self.vm.start()
+    def setUp(self):
+        self.app = Qubes()
+        self.vm = self.app.domains[self.vm_name]
+        # self._reboot()
+        if self.vm.is_running():
+            pass
+        else:
+            self.vm.start()
 
-  # def tearDown(self):
-  #   self.vm.shutdown()
+    # def tearDown(self):
+    #     self.vm.shutdown()
 
-  def _reboot(self):
-    try:
-      for v in self.vm.connected_vms.values():
-        if v.is_running():
-          print "Need to halt connected VM {} before testing".format(v)
-          v.shutdown()
-          while v.is_running():
+    def _reboot(self):
+        try:
+            for v in self.vm.connected_vms.values():
+                if v.is_running():
+                    msg = ("Need to halt connected VM {}"
+                           " before testing".format(v))
+                    print(msg)
+                    v.shutdown()
+                    while v.is_running():
+                        time.sleep(1)
+        except:
+            pass
+
+        if self.vm.is_running():
+            self.vm.shutdown()
+
+        while self.vm.is_running():
             time.sleep(1)
-    except:
-      pass
 
-    if self.vm.is_running():
-      self.vm.shutdown()
+        self.vm.start()
 
-    while self.vm.is_running():
-      time.sleep(1)
+    def _get_file_contents(self, path):
+        contents = subprocess.check_output(["qvm-run", "-p", self.vm_name,
+                                            "/bin/cat {}".format(path)])
+        return contents
 
-    self.vm.start()
+    def assertFilesMatch(self, remote_path, local_path):
+        remote_content = self._get_file_contents(remote_path)
 
-  def _get_file_contents(self, path):
-    contents = subprocess.check_output(["qvm-run", "-p", self.vm_name,
-                                        "/bin/cat {}".format(path)])
-    return contents
+        content = False
+        with open(local_path) as f:
+            content = f.read()
+        import difflib
+        print "".join(difflib.unified_diff(remote_content, content))
+        self.assertTrue(remote_content == content)
 
-  def assertFilesMatch(self, remote_path, local_path):
-    remote_content = self._get_file_contents(remote_path)
-
-    content = False
-    with open(local_path) as f:
-      content = f.read()
-    import difflib
-    print "".join(difflib.unified_diff(remote_content, content))
-    self.assertTrue(remote_content == content)
-
-  def assertFileHasLine(self, remote_path, line):
-    remote_content = self._get_file_contents(remote_path)
-    lines = remote_content.splitlines()
-    for l in lines:
-      if l == line:
-       return True
-    raise AssertionError("File {} does not contain expected line {}".format(remote_path, line))
+    def assertFileHasLine(self, remote_path, line):
+        remote_content = self._get_file_contents(remote_path)
+        lines = remote_content.splitlines()
+        for l in lines:
+            if l == line:
+                return True
+        msg = "File {} does not contain expected line {}".format(remote_path,
+                                                                 line)
+        raise AssertionError(msg)
--- tests/test_journalist_vm.py
+++ tests/test_journalist_vm.py
@@ -1,33 +1,34 @@
-from distutils import spawn
-
-import os
-import re
-import subprocess
-import time
 import unittest
 
 from base import SD_VM_Local_Test
 
+
 class SD_Journalist_Tests(SD_VM_Local_Test):
-  def setUp(self):
-    self.vm_name = "sd-journalist"
-    super(SD_Journalist_Tests, self).setUp()
+    def setUp(self):
+        self.vm_name = "sd-journalist"
+        super(SD_Journalist_Tests, self).setUp()
 
-  def test_move_to_svs(self):
-    self.assertFilesMatch("/usr/local/bin/move-to-svs", "sd-journalist/move-to-svs")
+    def test_move_to_svs(self):
+        self.assertFilesMatch("/usr/local/bin/move-to-svs",
+                              "sd-journalist/move-to-svs")
 
-  def test_sd_process_download(self):
-    self.assertFilesMatch("/usr/local/bin/sd-process-download", "sd-journalist/sd-process-download")
+    def test_sd_process_download(self):
+        self.assertFilesMatch("/usr/local/bin/sd-process-download",
+                              "sd-journalist/sd-process-download")
 
-  def test_do_not_open_here(self):
-    self.assertFilesMatch("/usr/local/bin/do-not-open-here", "sd-journalist/do-not-open-here")
+    def test_do_not_open_here(self):
+        self.assertFilesMatch("/usr/local/bin/do-not-open-here",
+                              "sd-journalist/do-not-open-here")
 
-  def test_sd_process_feedback(self):
-    self.assertFilesMatch("/usr/local/bin/sd-process-feedback", "sd-journalist/sd-process-feedback")
+    def test_sd_process_feedback(self):
+        self.assertFilesMatch("/usr/local/bin/sd-process-feedback",
+                              "sd-journalist/sd-process-feedback")
 
-  def test_sd_process_display(self):
-    self.assertFilesMatch("/usr/local/bin/sd-process-display", "sd-journalist/sd-process-display")
+    def test_sd_process_display(self):
+        self.assertFilesMatch("/usr/local/bin/sd-process-display",
+                              "sd-journalist/sd-process-display")
+
 
 def load_tests(loader, tests, pattern):
-  suite = unittest.TestLoader().loadTestsFromTestCase(SD_Journalist_Tests)
-  return suite
+    suite = unittest.TestLoader().loadTestsFromTestCase(SD_Journalist_Tests)
+    return suite
