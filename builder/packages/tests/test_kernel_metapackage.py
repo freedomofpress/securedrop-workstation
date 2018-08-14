f12bd2d21908e5132096a912c2c8b26f6b9a9641
mickael e <mickael@freedom.press>
2018-08-14 16:54:54 -0400

    Add tests for securedrop-workstation-grsec
    * Tests can only run locally, but in the future should be
    moved to a dedicated container
    * Cleaned up legacy molecule related files
    * Add clean makefile target
    * Change copyright to gplv3

--- builder/packages/tests/test_kernel_metapackage.py
+++ builder/packages/tests/test_kernel_metapackage.py
@@ -1,10 +1,32 @@
-import os
+import pytest
 
 KERNEL_VERSION = "4.14.53"
-
-DEB_FILE = "securedrop-workstation-grsec_{}-1_amd64.deb".format(KERNEL_VERSION)
+PACKAGE_NAME = "securedrop-workstation-grsec"
+PACKAGE_VERSION = "{}-1".format(KERNEL_VERSION)
+PACKAGE_ARCH = "amd64"
+DEB_FILE = "{}_{}_{}.deb".format(PACKAGE_NAME, PACKAGE_VERSION, PACKAGE_ARCH)
+KERNEL_DEPENDS = "linux-image-{}-grsec, linux-headers-{}-grsec, libelf-dev, paxctld".format(KERNEL_VERSION, KERNEL_VERSION)  # noqa: E501
+MAINTAINER = "SecureDrop Team <securedrop@freedom.press>"
 
 
 def test_hosts_file(host):
     f = host.file(DEB_FILE)
     assert f.exists
+
+
+@pytest.mark.parametrize("field,value", [
+    ("Version", PACKAGE_VERSION),
+    ("Depends", KERNEL_DEPENDS),
+    ("Maintainer", MAINTAINER),
+    ("Package", PACKAGE_NAME),
+    ("Architecture", PACKAGE_ARCH),
+])
+def test_control_fields(host, field, value):
+    c = "dpkg-deb -f securedrop-workstation-grsec_{}_amd64.deb {}".format(PACKAGE_VERSION, field)  # noqa: E501
+    assert host.check_output(c) == value
+
+
+def test_lintian(host):
+    if host.file("/usr/bin/lintian").exists:
+        cmd = host.run("lintian {}".format(DEB_FILE))
+        assert cmd.rc == 0
