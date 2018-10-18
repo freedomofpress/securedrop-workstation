c500fc1483c1475db3303faaacd69752e38ffb53
Conor Schaefer <conor@freedom.press>
2018-10-17 20:58:00 -0400

    Writes config tests for grsec kernels
    Tried to make the test DRY, covering:
    
      * HVM virtualization mode is set
      * Qubes-managed kernel disabled
      * Expected exact version of kernel is running
    
    Works well.

--- tests/test_vms_exist.py
+++ tests/test_vms_exist.py
@@ -4,6 +4,9 @@ from qubesadmin import Qubes
 from base import WANTED_VMS
 
 
+EXPECTED_KERNEL_VERSION = "4.14.74-grsec"
+
+
 class SD_VM_Tests(unittest.TestCase):
     def setUp(self):
         self.app = Qubes()
@@ -15,6 +18,21 @@ class SD_VM_Tests(unittest.TestCase):
         vm_set = set(self.app.domains)
         for test_vm in WANTED_VMS:
             self.assertTrue(test_vm in vm_set)
+
+    def _check_kernel(self, vm):
+        """
+        Confirms expected grsecurity-patched kernel is running.
+        """
+        # Running custom kernel requires HVM with empty kernel
+        self.assertTrue(vm.virt_mode == "hvm")
+        self.assertTrue(vm.kernel == "")
+
+        # Check exact kernel version in VM
+        raw_output = vm.run("uname -r")
+        # Response is a tuple of e.g. ('4.14.74-grsec\n', '')
+        kernel_version = raw_output[0].rstrip()
+        assert kernel_version.endswith("-grsec")
+        assert kernel_version == EXPECTED_KERNEL_VERSION
 
     def test_sd_whonix_config(self):
         vm = self.app.domains["sd-whonix"]
@@ -39,6 +57,7 @@ class SD_VM_Tests(unittest.TestCase):
         self.assertTrue(vm.template == "sd-svs-template")
         self.assertFalse(vm.provides_network)
         self.assertFalse(vm.template_for_dispvms)
+        self._check_kernel(vm)
 
     def test_sd_svs_disp_config(self):
         vm = self.app.domains["sd-svs-disp"]
@@ -47,6 +66,7 @@ class SD_VM_Tests(unittest.TestCase):
         self.assertTrue(vm.template == "sd-svs-disp-template")
         self.assertFalse(vm.provides_network)
         self.assertTrue(vm.template_for_dispvms)
+        self._check_kernel(vm)
 
     def test_sd_gpg_config(self):
         vm = self.app.domains["sd-gpg"]
@@ -56,13 +76,13 @@ class SD_VM_Tests(unittest.TestCase):
         self.assertTrue(vm.template == "sd-workstation-template")
         self.assertFalse(vm.provides_network)
         self.assertFalse(vm.template_for_dispvms)
+        self._check_kernel(vm)
 
     def test_sd_workstation_template(self):
         vm = self.app.domains["sd-workstation-template"]
         nvm = vm.netvm
         self.assertTrue(nvm is None)
-        self.assertTrue(vm.virt_mode == "hvm")
-        self.assertTrue(vm.kernel == "")
+        self._check_kernel(vm)
 
 
 def load_tests(loader, tests, pattern):
