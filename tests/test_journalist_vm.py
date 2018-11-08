49c0bb5a271ba5f0fdcafe082a9543a5fe2eb85b
redshiftzero <jen@freedom.press>
2018-11-07 18:10:01 -0800

    Update config tests

--- tests/test_journalist_vm.py
+++ tests/test_journalist_vm.py
@@ -9,25 +9,9 @@ class SD_Journalist_Tests(SD_VM_Local_Test):
         self.vm_name = "sd-journalist"
         super(SD_Journalist_Tests, self).setUp()
 
-    def test_move_to_svs(self):
-        self.assertFilesMatch("/usr/bin/move-to-svs",
-                              "sd-journalist/move-to-svs")
-
-    def test_sd_process_download(self):
-        self.assertFilesMatch("/usr/bin/sd-process-download",
-                              "sd-journalist/sd-process-download")
-
     def test_do_not_open_here(self):
         self.assertFilesMatch("/usr/bin/do-not-open-here",
                               "sd-journalist/do-not-open-here")
-
-    def test_sd_process_feedback(self):
-        self.assertFilesMatch("/usr/bin/sd-process-feedback",
-                              "sd-journalist/sd-process-feedback")
-
-    def test_sd_process_display(self):
-        self.assertFilesMatch("/usr/bin/sd-process-display",
-                              "sd-journalist/sd-process-display")
 
     def test_sd_proxy_package_installed(self):
         self.assertTrue(self._package_is_installed("securedrop-proxy"))
