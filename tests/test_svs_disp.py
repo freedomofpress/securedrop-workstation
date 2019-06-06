39c5a46a43e70468dfaba5d7e78c34f8a7fce1bf
Conor Schaefer <conor@freedom.press>
2019-06-06 10:31:12 -0700

    Adds config test to check for running services
    Used to determine whether paxctld is running inside the VMs. The most
    important is sd-svs-disp, since that's where we'll be installing
    libreoffice, and libreoffice requires pax flags to be set on java dependencies
    in order for the package to install successfully.
    
    Removes the previous implementation of a service check in favor of a
    more generalized approach. Overall, the config test suite should
    probably be reorganized.

--- tests/test_svs_disp.py
+++ tests/test_svs_disp.py
@@ -1,4 +1,3 @@
-import subprocess
 import unittest
 
 from base import SD_VM_Local_Test
@@ -15,12 +14,6 @@ class SD_SVS_Disp_Tests(SD_VM_Local_Test):
 
     def test_sd_svs_disp_libreoffice_installed(self):
         self.assertTrue(self._package_is_installed("libreoffice"))
-
-    def test_sd_svs_disp_paxctld_is_running(self):
-        cmd = ["qvm-run", "-p", "sd-svs-disp",
-               "/usr/sbin/service paxctld status"]
-        p = subprocess.check_output(cmd)
-        self.assertTrue("active (running)".encode() in p)
 
 
 def load_tests(loader, tests, pattern):
