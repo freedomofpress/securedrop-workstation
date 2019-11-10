59db42df9b10cebbe56722a27d73f320d9f57386
Conor Schaefer <conor@freedom.press>
2019-11-09 17:40:45 -0800

    Makes dom0 RPM repo config persistent
    The RPM repo pubkey must exist in sys-firewall, in order for
    `qubes-dom0-update` to pull in packages. We were previously adding the
    pubkey to sys-firewall in a temporary manner; now the pubkey persists
    across reboots.

--- tests/test_sys_firewall.py
+++ tests/test_sys_firewall.py
@@ -13,6 +13,10 @@ class SD_Sys_Firewall_Tests(SD_VM_Local_Test):
         self.assertFilesMatch("/etc/pki/rpm-gpg/RPM-GPG-KEY-securedrop-workstation-test",  # noqa
                               "sd-workstation/apt-test-pubkey.asc")
 
+    def test_rpm_repo_public_key(self):
+        self.assertFilesMatch("/rw/config/sd-copy-rpm-repo-pubkey.sh",
+                              "sys-firewall/sd-copy-rpm-repo-pubkey.sh")
+
 
 def load_tests(loader, tests, pattern):
     suite = unittest.TestLoader().loadTestsFromTestCase(SD_Sys_Firewall_Tests)
