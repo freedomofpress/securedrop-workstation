d0096b79ab2b6f995c4c6778cdf2735e229e628b
mickael e <mickael@freedom.press>
2019-11-11 09:34:26 -0500

    Add tests for sd-proxy and sd-whonix
    Ensure the apt lists for whonix repos are removed, per the action of `whonix_repository --disable`
    Also ensure this template change is propagated to the relevant AppVMs

--- tests/test_whonix_gw_14.py
+++ tests/test_whonix_gw_14.py
@@ -9,13 +9,8 @@ class Whonix_Gateway_14_Tests(SD_VM_Local_Test):
         self.whonix_apt_list = "/etc/apt/sources.list.d/whonix.list"
         super(Whonix_Gateway_14_Tests, self).setUp()
 
-    def test_whonix_gw_14_onion_repo_disabled(self):
-        whonix_apt_repo_disabled = "#deb tor+http://deb.dds6qkxpwdeubwucdiaord2xgbbeyds25rbsgr73tbfpqpt4a6vjwsyd.onion stretch main contrib non-free"  # noqa
-        self.assertFileHasLine(self.whonix_apt_list, whonix_apt_repo_disabled)
-
-    def test_whonix_gw_14_clearnet_repo_disabled(self):
-        whonix_apt_repo_disabled = "#deb https://deb.whonix.org stretch main contrib non-free"  # noqa
-        self.assertFileHasLine(self.whonix_apt_list, whonix_apt_repo_disabled)
+    def test_whonix_ws_14_repo_disabled(self):
+        assert self._fileExists(self.whonix_apt_list) is False
 
 
 def load_tests(loader, tests, pattern):
