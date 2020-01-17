8b8ea87b02235c889ac1ba90ad253dc2d9189e51
Erik Moeller <eloquence@gmail.com>
2020-01-17 13:52:40 -0800

    Edits mainly for brevity, formatting, punctuation tweaks

--- launcher/sdw_updater_gui/strings.py
+++ launcher/sdw_updater_gui/strings.py
@@ -1,38 +1,38 @@
 # checking for updates
 description_status_checking_updates = (
-    "<h3> Checking for Updates... </h3> <p>This should take around 3 to 5 minutes.</p>"
+    "<h3> Checking for updates... </h3> <p>This should take around 3 to 5 minutes.</p>"
 )
 
 # Results of update search
 description_status_updates_available = (
     "<h3> Security updates required. </h3><p>Before you can safely use the SecureDrop "
-    "app on your workstation, Qubes needs to download and run critical security updates. "
-    "<b> Updates should take around 5 to 10 minutes, depending on when was the last "
-    "time you updated. </b> Any interruption in the update process may break the "
-    "Workstation. Please close this window if now is a bad time.</p><p><b>The "
-    "workstation may need to be restarted after the updates are complete.</p>"
-    "<p>If you decide not to continue, please keep in mind that the updates will still "
-    "need to download and run before the SecureDrop app is opened again.</p>"
+    "app, Qubes needs to download and install critical security updates.</p>"
+    "<p>Updates should take around 5 to 10 minutes. The computer may need to be restarted "
+    "after updates are complete.</p>"
+    "<p><b>Any interruption in this process may break Workstation components.</p></b>"
+    "<p>Please close this window if now is a bad time, or click <em>Start Updates</em> "
+    "to continue.</p>"
 )
 description_status_up_to_date = (
-    "<h3> No updates, today !</h3>"
-    "<p>Click continue to launch the SecureDrop application.</p>"
+    "<h3> No updates today!</h3>"
+    "<p>Click <em>Continue</em> to launch the SecureDrop app.</p>"
 )
 description_error_check_updates_failed = (
     "<h3> Cannot check for updates </h3> There was an error retrieving updates. "
     "Please check your Internet connection. If this problem persists, "
-    "please contact your administrator"
+    "please contact your administrator."
 )
 # Applying updates
 description_status_applying_updates = (
     "<h3> Downloading and installing updates... </h3>"
-    "<p>VMs will be opening and closing as part of this progress, and you will see "
-    "a flood of notifications as part of this process.</p>"
-    "<h2>Please do not shutdown the Workstation at this time!<h2>"
+    "<p>You will see a flood of Qubes notifications as VMs are restarted "
+    "during this process.</p><br>"
+    "<h3>Do not shut down the computer or close this window until updating is "
+    "complete.</h3>"
 )
 description_status_updates_complete = (
     "<h3> All updates complete!</h3>"
-    "Click continue to safely launch and use the SecureDrop app. No reboot is necessary."
+    "Click <em>Continue</em> to launch the SecureDrop app. No reboot is necessary."
 )
 description_status_security_updates_failed = (
     "<h3> Security updates cannot be applied at this time</h3>"
@@ -42,14 +42,15 @@ description_status_security_updates_failed = (
 # Post-update actions (launching client, reboot)
 description_status_reboot_required = (
     "<h3> All updates complete!</h3>"
-    "A reboot will be required for security updates to be applied. "
-    "Please click the Reboot button below to reboot your workstation"
+    "A reboot is required for security updates to be applied. "
+    "Please click the <em>Reboot</em> button to continue."
 )
 description_status_rebooting = "<h3> Rebooting workstation</h3>"
 description_error_reboot = (
-    "<h3> Error rebooting workstation</h3>" "<p>Please contact your administrator.</p>"
+    "<h3> Error rebooting workstation</h3>"
+    "<p>Please contact your administrator.</p>"
 )
 description_error_lauching_client = (
-    "<h3> Error launching the SecureDrop application<h3>"
+    "<h3> Error launching the SecureDrop application</h3>"
     "Please contact your administrator."
 )
