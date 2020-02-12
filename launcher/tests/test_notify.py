c156c373950544227133fdcac432bcdb47cf1335
Erik Moeller <eloquence@gmail.com>
2020-02-11 16:55:21 -0800

    Add missing tests, cleaner mocks; avoid duplicate global

--- launcher/tests/test_notify.py
+++ launcher/tests/test_notify.py
@@ -41,6 +41,9 @@ BAD_TIMESTAMP_REGEX = r"Data in .* not in the expected format."
 # Regex for lock conflict with updater when launching notifier
 BUSY_LOCK_REGEX = r"Error obtaining lock on '.*'."
 
+# Regex for failure to obtain lock due to permission error
+LOCK_PERMISSION_REGEX = r"Error writing to lock file '.*'"
+
 
 @mock.patch("Notify.sdlog.error")
 @mock.patch("Notify.sdlog.warning")
@@ -52,8 +55,9 @@ def test_notify_lock(
     Test whether we can successfully obtain an exclusive lock for the notifier
     script
     """
-    with TemporaryDirectory() as tmpdir:
-        notify.LOCK_FILE_NOTIFIER = os.path.join(tmpdir, "sdw-launcher.lock")
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Notify.LOCK_FILE_NOTIFIER",
+                       os.path.join(tmpdir, "sdw-notify.lock")):
         pid = os.getpid()
         lh = notify.obtain_notify_lock()  # noqa: F841
         # No handled exception should occur
@@ -85,15 +89,16 @@ def test_updater_lock_prevents_notifier(
     from launching (so it does not come up when the user is in the process of
     updating).
     """
-    with TemporaryDirectory() as tmpdir:
-        notify.LOCK_FILE_LAUNCHER = os.path.join(tmpdir, "sdw-launcher.lock")
-        updater.LOCK_FILE = os.path.join(tmpdir, "sdw-launcher.lock")
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Updater.LOCK_FILE",
+                       os.path.join(tmpdir, "sdw-launcher.lock")):
+
         lh = updater.obtain_lock()  # noqa: F841
 
         # We're running in the same process, so obtaining a lock will succeed.
         # Instead we're mocking the IOError lockf would raise.
         with mock.patch("fcntl.lockf", side_effect=IOError()) as mocked_lockf:
-            can_get_lock = notify.can_obtain_updater_lock()
+            can_get_lock = notify.can_obtain_updater_lock(updater.LOCK_FILE)
             mocked_lockf.assert_called_once()
             assert can_get_lock is False
             error_string = mocked_error.call_args[0][0]
@@ -103,16 +108,77 @@ def test_updater_lock_prevents_notifier(
 @mock.patch("Notify.sdlog.error")
 @mock.patch("Notify.sdlog.warning")
 @mock.patch("Notify.sdlog.info")
-def test_no_updater_lock_has_no_effect(
+def test_notifier_lock_prevents_notifier(
+    mocked_info, mocked_warning, mocked_error
+):
+    """
+    Test whether an exlusive lock on the notifier lock file prevents the notifier
+    from launching again (to avoid multiple notifier windows).
+    """
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Notify.LOCK_FILE_NOTIFIER",
+                       os.path.join(tmpdir, "sdw-notify.lock")):
+
+        lh1 = notify.obtain_notify_lock()  # noqa: F841
+        # We're running in the same process, so obtaining a lock will succeed.
+        # Instead we're mocking the IOError lockf would raise.
+        with mock.patch("fcntl.lockf", side_effect=IOError()) as mocked_lockf:
+            lh2 = notify.obtain_notify_lock()
+            mocked_lockf.assert_called_once()
+            assert lh2 is None
+            error_string = mocked_error.call_args[0][0]
+            assert re.search(BUSY_LOCK_REGEX, error_string) is not None
+
+
+@mock.patch("Notify.sdlog.error")
+@mock.patch("Notify.sdlog.warning")
+@mock.patch("Notify.sdlog.info")
+def test_no_lockfile_has_no_effect(
         mocked_info, mocked_warning, mocked_error
 ):
     """
-    Test whether we _can_ run the notifier when we don't have a lock
-    on the updater.
+    Test whether we can run the notifier when a lockfile doesn't exist.
     """
     with TemporaryDirectory() as tmpdir:
-        notify.LOCK_FILE_LAUNCHER = os.path.join(tmpdir, "sdw-launcher.lock")
-        lock_result = notify.can_obtain_updater_lock()
+        lock_result = notify.can_obtain_updater_lock(os.path.join(tmpdir, "sdw-launcher.lock"))
+        assert lock_result is True
+
+
+@mock.patch("Notify.sdlog.error")
+@mock.patch("Notify.sdlog.warning")
+@mock.patch("Notify.sdlog.info")
+def test_permission_error_is_handled(
+        mocked_info, mocked_warning, mocked_error
+):
+    """
+    Test whether permission errors obtaining the lock are handled correctly
+    """
+    with mock.patch("builtins.open", side_effect=PermissionError()) as mocked_open:  # noqa: F821
+        lock = notify.obtain_notify_lock()
+        assert lock is None
+        mocked_open.assert_called_once()
+        mocked_error.assert_called_once()
+        error_string = mocked_error.call_args[0][0]
+        assert re.search(LOCK_PERMISSION_REGEX, error_string) is not None
+
+
+@mock.patch("Notify.sdlog.error")
+@mock.patch("Notify.sdlog.warning")
+@mock.patch("Notify.sdlog.info")
+def test_stale_lockfile_has_no_effect(
+        mocked_info, mocked_warning, mocked_error
+):
+    """
+    Test whether we can run the notifier when a lockfile exists, but nobody
+    is accessing it.
+    """
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Updater.LOCK_FILE",
+                       os.path.join(tmpdir, "sdw-launcher.lock")):
+
+        # Because we're not assigning the return value, it will be immediately released
+        updater.obtain_lock()
+        lock_result = notify.can_obtain_updater_lock(os.path.join(tmpdir, "sdw-launcher.lock"))
         assert lock_result is True
 
 
@@ -130,9 +196,11 @@ def test_warning_shown_if_uptime_exceeded_and_updater_never_ran(
     Test whether we're correctly going to show a warning if the uptime exceeds
     the warning threshold and the updater has never run
     """
-    with TemporaryDirectory() as tmpdir:
-        # We're going to look for a nonexistent file in an existing tmpdir
-        notify.LAST_UPDATED_FILE = os.path.join(tmpdir, "file-does-not-exist")
+    # We're going to look for a nonexistent file in an existing tmpdir
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Notify.LAST_UPDATED_FILE",
+                       os.path.join(tmpdir, "not-a-file")):
+
         with mock.patch("Notify.get_uptime_seconds") as mocked_uptime:
             mocked_uptime.return_value = uptime
             warning_should_be_shown = notify.is_update_check_necessary()
@@ -171,9 +239,10 @@ def test_warning_shown_if_warning_threshold_exceeded(
     threshold? Expected result varies based on whether system uptime exceeds
     a grace period (for the user to launch the app on their own).
     """
-    with TemporaryDirectory() as tmpdir:
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Notify.LAST_UPDATED_FILE",
+                       os.path.join(tmpdir, "sdw-last-updated")):
         # Write a "last successfully updated" date well in the past for check
-        notify.LAST_UPDATED_FILE = os.path.join(tmpdir, "sdw-last-updated")
         historic_date = datetime.date(2013, 6, 5).strftime(updater.DATE_FORMAT)
         with open(notify.LAST_UPDATED_FILE, "w") as f:
             f.write(historic_date)
@@ -206,9 +275,10 @@ def test_warning_not_shown_if_warning_threshold_not_exceeded(
     Another high priority case: we don't want to warn the user if they've
     recently run the updater successfully.
     """
-    with TemporaryDirectory() as tmpdir:
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Notify.LAST_UPDATED_FILE",
+                       os.path.join(tmpdir, "sdw-last-updated")):
         # Write current timestamp into the file
-        notify.LAST_UPDATED_FILE = os.path.join(tmpdir, "sdw-last-updated")
         just_now = datetime.datetime.now().strftime(updater.DATE_FORMAT)
         with open(notify.LAST_UPDATED_FILE, "w") as f:
             f.write(just_now)
@@ -230,8 +300,9 @@ def test_corrupt_timestamp_file_handled(
     The LAST_UPDATED_FILE must contain a timestamp in a specified format;
     if it doesn't, we return None and log the error.
     """
-    with TemporaryDirectory() as tmpdir:
-        notify.LAST_UPDATED_FILE = os.path.join(tmpdir, "sdw-last-updated")
+    with TemporaryDirectory() as tmpdir, \
+            mock.patch("Notify.LAST_UPDATED_FILE",
+                       os.path.join(tmpdir, "sdw-last-updated")):
         with open(notify.LAST_UPDATED_FILE, "w") as f:
             # With apologies to HAL 9000
             f.write("daisy, daisy, give me your answer do")
