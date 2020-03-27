f016c44d6ea1fa126795c0dd6d180df0245433f1
Conor Schaefer <conor@freedom.press>
2020-03-27 14:27:12 -0700

    Shuts down SDW VMs before sys-* VMs
    The updater logic now ensures that all SDW VMs are powered off *before*
    beginning to force-halt (via qvm-kill) the Fedora-based sys-* VMs.
    Doing so should help to reduce errors with SDW VMs coming back up.

--- launcher/sdw_updater_gui/Updater.py
+++ launcher/sdw_updater_gui/Updater.py
@@ -406,8 +406,20 @@ def shutdown_and_start_vms():
     in case they are being used by another non-workstation VM.
     """
 
+    sdw_vms_in_order = [
+        "sys-whonix",
+        "sd-proxy",
+        "sd-whonix",
+        "sd-app",
+        "sd-gpg",
+        "sd-log",
+    ]
+    sdlog.info("Shutting down SDW VMs for updates")
+    for vm in sdw_vms_in_order:
+        _safely_shutdown_vm(vm)
+
     sys_vms_in_order = ["sys-firewall", "sys-net", "sys-usb"]
-    sdlog.info("Rebooting system fedora-based VMs")
+    sdlog.info("Killing system fedora-based VMs for updates")
     for vm in sys_vms_in_order:
         try:
             subprocess.check_call(["qvm-kill", vm])
@@ -415,15 +427,12 @@ def shutdown_and_start_vms():
             sdlog.error("Error while killing {}".format(vm))
             sdlog.error(str(e))
 
+    sdlog.info("Starting system fedora-based VMs after updates")
     for vm in sys_vms_in_order:
         _safely_start_vm(vm)
 
-    vms_in_order = ["sys-whonix", "sd-proxy", "sd-whonix", "sd-app", "sd-gpg", "sd-log"]
-    sdlog.info("Rebooting all vms for updates")
-    for vm in vms_in_order:
-        _safely_shutdown_vm(vm)
-
-    for vm in vms_in_order:
+    sdlog.info("Starting SDW VMs after updates")
+    for vm in sdw_vms_in_order:
         _safely_start_vm(vm)
 
 
