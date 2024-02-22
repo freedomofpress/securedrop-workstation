c23ad3639f4a762f37c539200f19388c365437b6
Ro <ro@freedom.press>
2024-02-22 14:00:52 -0500

    Update sd-devices test to check for udisks2, not cryptsetup

--- tests/test_sd_devices.py
+++ tests/test_sd_devices.py
@@ -15,7 +15,7 @@ class SD_Devices_Tests(SD_VM_Local_Test):
         self.assertTrue(self._fileExists("/usr/share/mime/packages/application-x-sd-export.xml"))
 
     def test_sd_export_package_installed(self):
-        self.assertTrue(self._package_is_installed("cryptsetup"))
+        self.assertTrue(self._package_is_installed("udisks2"))
         self.assertTrue(self._package_is_installed("printer-driver-brlaser"))
         self.assertTrue(self._package_is_installed("printer-driver-hpcups"))
         self.assertTrue(self._package_is_installed("securedrop-export"))
