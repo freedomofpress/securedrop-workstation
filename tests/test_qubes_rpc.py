20ea5c6816d2b27637e6fd54ecd6e910054ebb66
Kunal Mehta <legoktm@debian.org>
2024-04-03 15:20:00 -0400

    Update qubes-rpc tests
    Just verify the new policy files are installed, don't do any verification
    of contents yet.

--- tests/test_qubes_rpc.py
+++ tests/test_qubes_rpc.py
@@ -1,50 +1,13 @@
-import io
 import os
 import unittest
-import yaml
 
 
-# TODO: update for new policy format/layout
+# TODO: add actual integration testing of policies
 class SD_Qubes_Rpc_Tests(unittest.TestCase):
-    def setUp(self):
-        self.expected = self._loadVars()
-
-    def tearDown(self):
-        pass
-
-    def test_Policies(self):
-        # Using a for loop instead of pytest.parametrize due to
-        # the absence of pytest in dom0.
-        fail = False
-        for policy in self.expected:
-            if not self._startsWith(policy["policy"], policy["starts_with"]):
-                fail = True
-        self.assertFalse(fail), "Policy does not match: " + policy["policy"]
-
-    def _startsWith(self, filename, expectedPolicy):
-        filePath = filename
-        # The Qubes 4.0 grants assumed all policies were in the same dir.
-        # Support that for backwards-compatibility on 4.0, but we'll use
-        # absolute paths in the test vars from 4.1 onward.
-        if not filePath.startswith("/"):
-            filePath = os.path.join("/etc/qubes-rpc/policy", filename)
-
-        with io.open(filePath, "r") as f:
-            actualPolicy = f.read()
-            if actualPolicy.startswith(expectedPolicy):
-                return True
-            else:
-                print("\n\n#### BEGIN RPC policy error report ####\n\n")
-                print("Policy for {} is:\n{}".format(filename, actualPolicy))
-                print("Policy for {} should be:\n{}".format(filename, expectedPolicy))
-                print("\n\n#### END RPC policy error report ####\n\n")
-                return False
-
-    def _loadVars(self):
-        filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vars", "qubes-rpc.yml")
-        with io.open(filepath, "r") as f:
-            data = yaml.safe_load(f)
-        return data
+    def test_policies(self):
+        """verify the policies are installed"""
+        assert os.path.exists("/etc/qubes/policy.d/70-securedrop-workstation.policy")
+        assert os.path.exists("/etc/qubes/policy.d/60-securedrop-workstation.policy")
 
 
 def load_tests(loader, tests, pattern):
