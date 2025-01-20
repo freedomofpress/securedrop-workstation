09309dea7e718fab5ebec49754286d8552ac73e5
Rowen S <ro@freedom.press>
2025-01-20 11:05:39 -0500

    fixup

--- tests/test_dom0_systemd.py
+++ tests/test_dom0_systemd.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import subprocess
-import unittest
 
 # .spec file shows which units are shipped with rpm
 SYSTEM_UNITS = ["securedrop-logind-override-disable.service"]
@@ -19,88 +18,117 @@ SYSTEMD_LIST_UNITS = ["systemctl", "--legend=false", "list-unit-files", "--all",
 SYSTEMD_EXIT_SUCCESS = "status=0/SUCCESS"
 
 
-class Dom0_Systemd(unittest.TestCase):
-    def _systemd_verify_sd_units(
-        self, services: list[str] = None, is_userspace: bool = False
-    ) -> bool:
-        args = ["systemd-analyze"]
-        if is_userspace:
-            args.append("--user")
-        if services:
-            args.append("verify")
-            args.extend(services)
-        try:
-            # systemd-analyze [--user] verify [service1.service service2.service]
-            # exits 0 on success
-            subprocess.check_call(args)
-            return True
-        except subprocess.CalledProcessError:
-            return False
+class Dom0TestException(Exception):
+    pass
 
-    def _systemd_list_sd_units(self, is_userspace: bool = False) -> str:
-        args = SYSTEMD_LIST_UNITS
-        if is_userspace:
-            args.append("--user")
-        try:
-            return subprocess.check_output(args).decode()
-        except subprocess.CalledProcessError:
-            self.fail("Error listing units")
 
-    def _get_unit_last_status(self, unit: str, is_userspace: bool = False) -> tuple[str, str]:
-        args = ["systemctl", "status", unit]
-        if is_userspace:
-            args.append("--user")
-        status = None
-        try:
-            status = subprocess.check_output(args).decode()
-        except subprocess.CalledProcessError:
-            self.fail(f"Error checking status for {unit}")
+def systemd_verify_sd_units(services: list[str] = None, is_userspace: bool = False) -> bool:
+    """
+    Helper. systemd-analyze services to check if well-formed.
+    If no units specified, `systemd-analyze` will run without
+    the `verify` command.
 
-        if status:
-            # Parse output and return tuple of (code, status)
-            status_lines = status.split("\n")
-            for line in status_lines:
-                if line.startswith("Main PID"):
-                    # format:
-                    # "Main PID: $pid (code=code, status=retcode/STATUS)"
-                    return line.split("(")[-1].replace(")", "").split(", ")
-
-        self.fail("Could not parse status")
-        return None
-
-    def _units_present_enabled(self, expected_units: list[str], is_userspace: bool = False):
-        # Format of systemctl list-unit-files is UNIT_FILE\tSTATE\tPRESET
-        unit_count = 0
-        units = self._systemd_list_sd_units(is_userspace=is_userspace).split("\n")
-        for unit_and_status in units:
-            unit = unit_and_status.split()
-            if unit not in expected_units:
-                self.fail(f"Unexpected unit file {unit[0]}")
-            else:
-                # Right now, all units are enabled
-                unit_count += 1
-                if unit[1] != "enabled":
-                    self.fail(f"{unit[0]} is not enabled")
-
-                if self._get_unit_last_status(unit[0], is_userspace) is not SYSTEMD_EXIT_SUCCESS:
-                    self.fail(f"{unit[0]} not {SYSTEMD_EXIT_SUCCESS}")
-
-        expected = len(expected_units)
-        if unit_count != expected:
-            self.fail(f"Missing units (got {unit_count}, need {expected})")
-
-        # If we got here, we have exactly the correct number of units, and
-        # they are all enabled, and their last runs were all successful
+    Option to run against user units via `is_userspace` boolean.
+    """
+    args = ["systemd-analyze"]
+    if is_userspace:
+        args.append("--user")
+    if services:
+        args.append("verify")
+        args.extend(services)
+    try:
+        # systemd-analyze [--user] verify [service1.service service2.service]
+        # exits 0 on success
+        subprocess.check_call(args)
         return True
+    except subprocess.CalledProcessError:
+        return False
 
-    def test_system_units_well_formed(self):
-        assert self._systemd_verify_sd_units(SYSTEM_UNITS)
 
-    def test_user_units_well_formed(self):
-        assert self._systemd_verify_sd_units(USER_UNITS, is_userspace=True)
+def systemd_list_sd_units(is_userspace: bool = False) -> str | None:
+    """
+    Helper. List systemd units and pattern-match. Option to specify user units.
+    """
+    args = SYSTEMD_LIST_UNITS
+    if is_userspace:
+        args.append("--user")
+    try:
+        return subprocess.check_output(args).decode()
+    except subprocess.CalledProcessError:
+        user_flag = "user" if is_userspace else ""
+        raise Dom0TestException(f"Failed to list {user_flag} units")
 
-    def test_system_units_present_enabled(self):
-        self._units_present_enabled(SYSTEM_UNITS)
 
-    def test_user_units_present_enabled(self):
-        self._units_present_enabled(USER_UNITS, is_userspace=True)
+def get_unit_last_status(unit: str, is_userspace: bool = False) -> tuple[str, str]:
+    """
+    Helper. Given a systemd unit (.service/.timer), parse last
+    unit status. Option to specify user unit via is_userspace flag.
+    """
+    args = ["systemctl", "status", unit]
+    if is_userspace:
+        args.append("--user")
+    status = None
+    try:
+        status = subprocess.check_output(args).decode()
+    except subprocess.CalledProcessError:
+        raise Dom0TestException(f"Failed to get last unit status: {unit}")
+
+    if status:
+        # Parse output and return tuple of (code, status)
+        status_lines = status.split("\n")
+        for line in status_lines:
+            if line.startswith("Main PID"):
+                # format:
+                # "Main PID: $pid (code=code, status=retcode/STATUS)"
+                return line.split("(")[-1].replace(")", "").split(", ")
+
+    raise Dom0TestException("Could not parse status")
+
+
+def units_present_enabled(expected_units: list[str], is_userspace: bool = False):
+    """
+    Helper. Check list of expected units and assert that they are
+    enabled. Option to specify user units.
+    """
+    # Format of systemctl list-unit-files is UNIT_FILE\tSTATE\tPRESET
+    unit_count = 0
+    units = systemd_list_sd_units(is_userspace=is_userspace).split("\n")
+    for unit_and_status in units:
+        unit = unit_and_status.split()
+        if unit not in expected_units:
+            raise Dom0TestException(f"Unexpected unit file {unit[0]}")
+        else:
+            # Right now, all units are enabled
+            unit_count += 1
+            if unit[1] != "enabled":
+                raise Dom0TestException(f"{unit[0]} is not enabled")
+
+            if get_unit_last_status(unit[0], is_userspace) is not SYSTEMD_EXIT_SUCCESS:
+                raise Dom0TestException(f"{unit[0]} not {SYSTEMD_EXIT_SUCCESS}")
+
+    expected = len(expected_units)
+    if unit_count != expected:
+        raise Dom0TestException(f"Missing units (got {unit_count}, need {expected})")
+
+    # If we got here, we have exactly the correct number of units, and
+    # they are all enabled, and their last runs were all successful
+    return True
+
+
+### Begin Tests ###
+
+
+def test_system_units_well_formed():
+    assert systemd_verify_sd_units(SYSTEM_UNITS)
+
+
+def test_user_units_well_formed():
+    assert systemd_verify_sd_units(USER_UNITS, is_userspace=True)
+
+
+def test_system_units_present_enabled():
+    units_present_enabled(SYSTEM_UNITS)
+
+
+def test_user_units_present_enabled():
+    units_present_enabled(USER_UNITS, is_userspace=True)
