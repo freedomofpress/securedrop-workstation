19aa360b278d8f27dd1d50544f1686237696b871
Kunal Mehta <legoktm@debian.org>
2026-02-20 15:54:56 -0500

    Refactor GetJournalistSecretKeys.py
    * All caps for global constants, move SECRET_KEYPATH into one
    * log -> logger

--- files/securedrop.GetJournalistSecretKeys.py
+++ files/securedrop.GetJournalistSecretKeys.py
@@ -9,36 +9,36 @@ from pathlib import Path
 
 from systemd.journal import JournalHandler
 
+# TODO: add support for multiple keys
+SECRET_KEY_PATH = Path("/usr/share/securedrop-workstation-dom0-config/sd-journalist.sec")
+
 # Logging set up
-rpc_policy_name = Path(__name__).name
-log = logging.getLogger(rpc_policy_name)
-log.setLevel(logging.INFO)
+RPC_POLICY_NAME = Path(__name__).name
+logger = logging.getLogger(RPC_POLICY_NAME)
+logger.setLevel(logging.INFO)
 
 # Make log available in dom0 for auditing (via systemd journal)
-log.addHandler(JournalHandler(SYSLOG_IDENTIFIER=rpc_policy_name))
+logger.addHandler(JournalHandler(SYSLOG_IDENTIFIER=RPC_POLICY_NAME))
 
 # Also log to stderr so error is visible on calling qube
-log.addHandler(logging.StreamHandler(sys.stdout))
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 def main():
     source_qube = os.getenv("QREXEC_REMOTE_DOMAIN")
     if source_qube != "sd-gpg":
         # Extra due-dilligence in case there is a policy misconfiguration
-        log.error(f"Security violation: attempted call from qube {source_qube}")
+        logger.error(f"Security violation: attempted call from qube {source_qube}")
         sys.exit(1)
 
-    # TODO: add support for multiple keys
-    secret_key_path = Path("/usr/share/securedrop-workstation-dom0-config/sd-journalist.sec")
-
-    if not secret_key_path.exists():
+    if not SECRET_KEY_PATH.exists():
         # Log to systemd so we can
-        log.error(f"Error: secret key file not found in '{secret_key_path}'")
+        logger.error(f"Error: secret key file not found in '{SECRET_KEY_PATH}'")
         sys.exit(2)
 
     # Output the contents of the secret keys file
     # This will be sent back to the calling VM via stdout
-    with open(secret_key_path) as secret_key_f:
+    with open(SECRET_KEY_PATH) as secret_key_f:
         print(secret_key_f.read().strip())
 
 
