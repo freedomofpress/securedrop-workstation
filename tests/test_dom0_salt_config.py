a87cacb1300c0bfd67f4753706d9b8be9ee613f5
Kunal Mehta <legoktm@debian.org>
2026-03-02 14:49:27 -0500

    Run test_trust_desktop_launcher as non-root user
    Needed for OpenQA, which runs pytest as root.

--- tests/test_dom0_salt_config.py
+++ tests/test_dom0_salt_config.py
@@ -20,24 +20,42 @@ def test_is_topfile_enabled():
 
 @skip_on_qubes_4_2
 @pytest.mark.provisioning
-def test_trust_desktop_launcher():
+def test_trust_desktop_launcher(run_as_gui_user):
     # salt should have already provisioned the correct checksum
-    res = subprocess.run(["/usr/bin/securedrop/sdw-desktop-trust", "--check"], check=False)
+    res = subprocess.run(
+        run_as_gui_user + ["/usr/bin/securedrop/sdw-desktop-trust", "--check"], check=False
+    )
     assert res.returncode == 0, "desktop launcher is correctly trusted"
 
     desktop_file = Path.home() / "Desktop" / "press.freedom.SecureDropUpdater.desktop"
     # Manually modify the checksum to a clearly invalid value
     subprocess.check_call(
-        ["gio", "set", "-t", "string", str(desktop_file), "metadata::xfce-exe-checksum", "invalid"]
+        run_as_gui_user
+        + [
+            "gio",
+            "set",
+            "-t",
+            "string",
+            str(desktop_file),
+            "metadata::xfce-exe-checksum",
+            "invalid",
+        ]
     )
 
-    res = subprocess.run(["/usr/bin/securedrop/sdw-desktop-trust", "--check"], check=False)
+    res = subprocess.run(
+        run_as_gui_user + ["/usr/bin/securedrop/sdw-desktop-trust", "--check"], check=False
+    )
     assert res.returncode == 1, "desktop launcher checksum shouldn't match"
 
     res = subprocess.run(
-        ["/usr/bin/securedrop/sdw-desktop-trust"], text=True, check=False, capture_output=True
+        run_as_gui_user + ["/usr/bin/securedrop/sdw-desktop-trust"],
+        text=True,
+        check=False,
+        capture_output=True,
     )
     assert "sdw-desktop-trust: set xfce-exe-checksum on" in res.stdout
 
-    res = subprocess.run(["/usr/bin/securedrop/sdw-desktop-trust", "--check"], check=False)
+    res = subprocess.run(
+        run_as_gui_user + ["/usr/bin/securedrop/sdw-desktop-trust", "--check"], check=False
+    )
     assert res.returncode == 0, "desktop launcher is correctly trusted"
