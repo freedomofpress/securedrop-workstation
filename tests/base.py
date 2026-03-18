b72e61f9663761609c2f50696a37336c6ac1bf30
deeplow <francisco@freedom.press>
2026-03-18 15:47:13 +0000

    Add tests for sd-printers and fix sd-devices tests
    Re-uses the tests/test_vm_sd_devices.py to re-use some of the
    functionality. To make the common tests work (which assume a qube
    fixture), we're parameterizing that fixture. This way the tests that
    import the 'qube' fixture will run on both 'sd-devices' and
    'sd-printers' and the ones with 'qube_sd_devices' and 'qube_sd_printers'
    will only run on the respective one.
    
    (cherry picked from commit cbb24a9962ba4923b938a820b56587a458d16d37)

--- tests/base.py
+++ tests/base.py
@@ -20,7 +20,7 @@ SD_TEMPLATE_SMALL = f"sd-small-{DEBIAN_VERSION}-template"
 SD_TAG = "sd-workstation"  # Tag identifying SecureDrop Workstation-managed VMs
 
 # Expectations regarding VMs' existence and versions
-SD_VMS = ["sd-gpg", "sd-log", "sd-proxy", "sd-app", "sd-viewer", "sd-devices"]
+SD_VMS = ["sd-gpg", "sd-log", "sd-proxy", "sd-app", "sd-viewer", "sd-devices", "sd-printers"]
 SD_DVM_TEMPLATES = ["sd-devices-dvm", "sd-proxy-dvm"]
 SD_TEMPLATES = [SD_TEMPLATE_BASE, SD_TEMPLATE_LARGE, SD_TEMPLATE_SMALL]
 SD_UNTAGGED_DEPRECATED_VMS = ["sd-retain-logvm"]
