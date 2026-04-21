2378149fe9f7cf4007c29beafdd34d33f067047c
deeplow <francisco@freedom.press>
2026-04-21 16:09:58 +0100

    Set xfce-exe-checksum on desktop launcher
    XFCE has a new "security" feature that requires us to set a checksum
    of the .desktop file in the gio database before it can be used [1].
    
    Fixes #1577
    
    [1]: https://forum.xfce.org/viewtopic.php?id=16407
    
    Includes the following follow-up commits appending during review.
    
    * Test that dom0 XFCE desktop files are launchable
    * fix: shellcheck
    * lint: add type annotation for new test
      Doing this proactively, as #1625 is currently under review.
    * fix(test): explicitly set dbus session in tests
      Resolves a test failure that was specific to OpenQA,
      due to a subtlety in the execution context.
      The shell script being tested constructs its own
      definition of the dbus session path, so the test
      must do the same.
    * fix: add new script subcommand to usage info
      Also sorted the subcommands for readability.

--- tests/test_dom0_salt_config.py
+++ tests/test_dom0_salt_config.py
@@ -1,6 +1,17 @@
+import hashlib
+import os
 import subprocess
+from pathlib import Path
 
 import pytest
+
+from tests.conftest import skip_on_qubes_4_2
+
+DESKTOP_FILE_NAME = "press.freedom.SecureDropUpdater.desktop"
+
+
+def checksum(path: Path) -> str:
+    return hashlib.sha256(path.read_bytes()).hexdigest()
 
 
 def test_is_topfile_enabled():
@@ -13,3 +24,25 @@ def test_is_topfile_enabled():
 
     except subprocess.CalledProcessError:
         pytest.fail("Error checking topfiles")
+
+
+@skip_on_qubes_4_2
+@pytest.mark.provisioning
+def test_trust_desktop_launcher() -> None:
+    desktop_file = Path.home() / "Desktop" / DESKTOP_FILE_NAME
+
+    # Duplicate the dbus session bootstrap logic from `../files/update-xfce-settings`,
+    # so the test works under invocations that don't inherit a session bus,
+    # such as OpenQA's `su user -c`.
+    env = {**os.environ, "DBUS_SESSION_BUS_ADDRESS": f"unix:path=/run/user/{os.getuid()}/bus"}
+
+    # salt should have already provisioned the correct checksum
+    res = subprocess.run(
+        ["gio", "info", "--attributes", "metadata::xfce-exe-checksum", desktop_file],
+        text=True,
+        check=False,
+        capture_output=True,
+        env=env,
+    )
+    assert res.returncode == 0, "desktop launcher does not match"
+    assert checksum(desktop_file) in res.stdout
