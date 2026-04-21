ddd0e238e3ed8794fe422c89f27b1a47b1d7056d
deeplow <francisco@freedom.press>
2026-04-21 16:58:26 +0100

    Test that dom0 XFCE desktop files are launchable

--- tests/test_dom0_salt_config.py
+++ tests/test_dom0_salt_config.py
@@ -1,6 +1,16 @@
+import hashlib
 import subprocess
+from pathlib import Path
 
 import pytest
+
+from tests.conftest import skip_on_qubes_4_2
+
+DESKTOP_FILE_NAME = "press.freedom.SecureDropUpdater.desktop"
+
+
+def checksum(path: Path) -> str:
+    return hashlib.sha256(path.read_bytes()).hexdigest()
 
 
 def test_is_topfile_enabled():
@@ -13,3 +23,19 @@ def test_is_topfile_enabled():
 
     except subprocess.CalledProcessError:
         pytest.fail("Error checking topfiles")
+
+
+@skip_on_qubes_4_2
+@pytest.mark.provisioning
+def test_trust_desktop_launcher():
+    desktop_file = Path.home() / "Desktop" / DESKTOP_FILE_NAME
+
+    # salt should have already provisioned the correct checksum
+    res = subprocess.run(
+        ["gio", "info", "--attributes", "metadata::xfce-exe-checksum", desktop_file],
+        text=True,
+        check=False,
+        capture_output=True,
+    )
+    assert res.returncode == 0, "desktop launcher does not match"
+    assert checksum(desktop_file) in res.stdout
