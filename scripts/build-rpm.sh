018f74fec7573bbede8d032768c36da3387d0cd0
Kevin O'Gorman <kog@freedom.press>
2026-09-04 14:53:42 -0400

    Build admin rpm along with workstation rpm by default

--- scripts/build-rpm.sh
+++ scripts/build-rpm.sh
@@ -17,6 +17,7 @@ trap 'find . -type f -exec touch -m {} \;' EXIT
 
 rpmbuild \
     --build-in-place \
+    --with admin \
     --define "_topdir $PWD/rpm-build" \
     -bb "rpm-build/SPECS/${PROJECT}.spec"
 
@@ -24,4 +25,4 @@ rpmbuild \
 python3 scripts/verify_rpm_mtime.py
 
 printf '\nBuild complete! RPMs and their checksums are:\n\n'
-find rpm-build/ -type f -iname "${PROJECT}-$(cat "${TOPLEVEL}/VERSION")*.rpm" -print0 | sort -zV | xargs -0 sha256sum
+find rpm-build/ -type f -iname "securedrop-*-$(cat "${TOPLEVEL}/VERSION")*.rpm" -print0 | sort -zV | xargs -0 sha256sum
